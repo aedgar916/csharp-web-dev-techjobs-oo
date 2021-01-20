0e0aab69784809ba181c2029a1b5010aac060251
Amanda Edgar <plainjane916@gmail.com>
2021-01-20 16:43:06 -0600

    Created TechJobsTests for unit testing.

---
+++ TechJobsTests/JobTests.cs
@@ -0,0 +1,50 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
+
+namespace TechJobsTests
+{
+    [TestClass]
+    public class JobTests
+    {
+        //[TestMethod]
+        //public void EmptyTest()
+        //{
+        //    Assert.AreEqual(true, true);
+        //}
+
+        [TestMethod]
+        public void TestSettingJobId()
+        {
+            Job job1 = new Job();
+            Job job2 = new Job();
+
+            Assert.AreEqual(true, job1.Id != job2.Id);
+            Assert.AreEqual(1, job2.Id - job1.Id);
+        }
+
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+            Assert.AreNotEqual(null, job.Id);
+            Assert.AreEqual("Product tester", job.Name);
+            Assert.AreEqual("ACME", job.EmployerName.Value);
+            Assert.AreEqual("Desert", job.EmployerLocation.Value);
+            Assert.AreEqual("Quality control", job.JobType.Value);
+            Assert.AreEqual("Persistence", job.JobCoreCompetency.Value);
+       
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+            Assert.AreEqual(false, job1.Equals(job2));
+        }
+
+    }
+}
